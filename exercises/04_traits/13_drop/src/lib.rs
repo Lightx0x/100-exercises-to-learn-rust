36a6cd52ce08dfde70a91c217cad8c610f25d60f
lightx0x <kingcairo4000@gmail.com>
2026-05-31 11:29:51 +0100

    Learn Copy, Clone and Drop

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -1,6 +1,30 @@
 // TODO: implement a so-called "Drop bomb": a type that panics when dropped
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
+pub struct DropBomb {
+    armed: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> DropBomb {
+        DropBomb {
+            armed: true,
+        }
+    }
+
+    pub fn defuse(&mut self) {
+        self.armed = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.armed {
+            panic!("I have been dropped!"); 
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -10,6 +34,7 @@ mod tests {
     #[should_panic]
     fn test_drop_bomb() {
         let bomb = DropBomb::new();
+        drop(bomb);
         // The bomb should panic when dropped
     }
 
@@ -17,6 +42,7 @@ mod tests {
     fn test_defused_drop_bomb() {
         let mut bomb = DropBomb::new();
         bomb.defuse();
+        drop(bomb);
         // The bomb should not panic when dropped
         // since it has been defused
     }
